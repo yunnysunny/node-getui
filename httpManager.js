cc07563b89225029038290677619ac8344e6cccc
yunnysunny <yunnysunny@gmail.com>
2016-05-09 15:15:31 +0800

    code format

--- httpManager.js
+++ httpManager.js
@@ -14,46 +14,47 @@ var httpManager = {
      */
     post: function (host, postData, needGzip, callback) {
         postData.version = GtConfig.getSDKVersion();
-		var tries = GtConfig.getHttpTryCount(); // 最大重连次数
+        var tries = GtConfig.getHttpTryCount(); // 最大重连次数
         attempt(host, tries);
         function attempt(ho, times) {
-			var options = {
-				uri: ho,
-				method: 'post',
-				timeout: GtConfig.getHttpSoTimeOut(),
-				rejectUnauthorized: false,
-				headers: {
-					'Content-Type': 'text/html;charset=UTF-8',
-					'User-Agent': 'Getui nodejs',
-					'Accept': '*/*'
-				}
-			};
+            var options = {
+                uri: ho,
+                method: 'post',
+                timeout: GtConfig.getHttpSoTimeOut(),
+                rejectUnauthorized: false,
+                headers: {
+                    'Content-Type': 'text/html;charset=UTF-8',
+                    'User-Agent': 'Getui nodejs',
+                    'Accept': '*/*'
+                }
+            };
             postData = postData || {};
             var action = postData['action'];
             if (action != null && action.length > 0) {
                 options.headers['Gt-Action'] = action;
             }
-			if (needGzip) {
-				options.gzip = true;
-				options.headers['Content-Encoding'] = 'gzip';
-				return zlib.gzip(JSON.stringify(postData),function(err,buf) {
+            if (needGzip) {
+                options.gzip = true;
+                options.headers['Content-Encoding'] = 'gzip';
+                return zlib.gzip(JSON.stringify(postData), function (err, buf) {
                     if (err) {
-						console.error('gzip error:', err);
-                        if(--times){
-							return attempt(host, times);
-						}
-						return callback && callback(err,null);
+                        console.error('gzip error:', err);
+                        if (--times) {
+                            return attempt(host, times);
+                        }
+                        return callback && callback(err, null);
                     }
                     options.body = buf;
-                    doRequest(options,times);
+                    doRequest(options, times);
                 });
-			}
+            }
 
-			options.json = true;
-			options.body = postData;
+            options.json = true;
+            options.body = postData;
             doRequest(options);
         }
-        function doRequest(options,times) {
+
+        function doRequest(options, times) {
             request(
                 options,
                 function (err, res, data) {
@@ -66,7 +67,7 @@ var httpManager = {
                         callback && callback(null, data);
                         //console.log("callback over");
                     } else if (--times) {
-                        console.error(err,res ? res.statusCode : 'unknown');
+                        console.error(err, res ? res.statusCode : 'unknown');
                         attempt(host, times);
                     } else {
                         callback && callback(err, null);
